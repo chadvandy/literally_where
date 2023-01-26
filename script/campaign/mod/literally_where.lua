219b1ee7d180a8bb370589141e32b1299d786af0
vandy <chadvandewettering@gmail.com>
2023-01-26 11:04:52 -0500

    init commit

---
+++ script/campaign/mod/literally_where.lua
@@ -0,0 +1,9 @@
+--- Create a new class object!
+---@type fun(className:string,attr:table) : Class
+local new_class = require "script.groovy.includes.30-log"
+
+---@class WorldObject : Class
+local defaults = {}
+
+---@class WorldObject : Class
+local WorldObject = new_class("WorldObject", defaults)
